dd39154450113783b2852afd7d523a16b530cb90
Aayush Rijal <arijal@aiims.com.au>
2023-11-07 12:07:42 +1100

    all pages done

--- themes/commercial-plumbing.php
+++ themes/commercial-plumbing.php
@@ -30,8 +30,176 @@ require 'inc/nav.php';
 require 'inc/serviceBanner.php';
 ?>
 
-<main class="innerServicePage">
-    
+<main class="commercialPlumbingServicePage">
+    <section class="help">
+        <div class="container">
+            <section class="introduction">
+                <div class="row align-items-center">
+                    <div class="col-6">
+                        <article>
+                            <h3 class="fs-55 fw-500 lh-1 text-capitalize fw-700">Keep up the flow for your business</h3>
+                            <article class="fs-16 description lh-1_67 py-4">
+                                <p>Rapid Plumbing Group is the plumber commercial and industrial businesses need! Cash isn’t the only thing that should be constantly flowing for a business; functional plumbing is a basic need for workers and customers alike. Any disruptions easily slow down your employees, and reduce customer satisfaction. Worst case scenario, work might grind to a halt if something such as a water heater fails. These things must be avoided at all costs.</p>
+                                <p>At Rapid Plumbing Group, we offer reliable solutions for businesses of all sizes. Our team of licensed plumbers has the expertise and experience necessary to handle plumbing, gas, and more services. We also make sure all your systems are up to code. We are available 24/7, so we can help resolve issues before or after business hours. Keep Rapid Plumbing Group on hand to look after your business’ plumbing.</p>
+                            </article>
+                            <a href="#" class="btn btn-primary text-white rounded-pill px-3 px-lg-5 fs-18 fw-700 d-inline-flex">Enquire</a>
+                        </article>
+                    </div>
+                    <div class="col-6">
+                        <img src="<?= V_CDN_URL . V_THEME_DIR ?>_assets/images/lib/commercial-plumbing.jpg" alt="" />
+                    </div>
+                </div>
+            </section>
+        </div>
+
+        <div class="container pt-7">
+            <div class="row gy-4">
+                <div class="col-12">
+                    <article class="aboutCard">
+                        <div class="row h-100">
+                            <div class="col-6">
+                                <img src="<?= V_CDN_URL . V_THEME_DIR ?>_assets/images/lib/family-owned-and-run.png" alt="" class="mainImg" />
+                            </div>
+                            <div class="col-6 d-flex align-items-center">
+                                <article class="p-5">
+                                    <h3 class="fs-60 fw-600 lh-1">Family-Owned, Family-Run</h3>
+                                    <p class="fs-18 pt-3 lh-1_5">
+                                        We started as a family business with humble beginnings from Penrith, and have continued to grow both as a business and as people. Our team is family to us, and so is the local community. We take good care of our own, and that includes you and your needs.
+                                    </p>
+                                </article>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                <div class="col-12">
+                    <article class="aboutCard">
+                        <div class="row h-100">
+                            <div class="col-6">
+                                <img src="<?= V_CDN_URL . V_THEME_DIR ?>_assets/images/lib/speed.png" alt="" class="mainImg" />
+                            </div>
+                            <div class="col-6 d-flex align-items-center">
+                                <article class="p-5">
+                                    <h3 class="fs-60 fw-600 lh-1">Speed</h3>
+                                    <p class="fs-18 pt-3 lh-1_5">
+                                        Rapid Plumbing Group has worked on large-scale projects with big clients, such as NSW Fire Services, schools across the state, and various local councils. We have consistently exceeded expectations by outperforming KPIs and finishing well before deadlines. Naturally, you can expect the same speed on your demands.
+                                    </p>
+                                </article>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                <div class="col-12">
+                    <article class="aboutCard">
+                        <div class="row h-100">
+                            <div class="col-6">
+                                <img src="<?= V_CDN_URL . V_THEME_DIR ?>_assets/images/lib/quality-customer-service.png" alt="" class="mainImg" />
+                            </div>
+                            <div class="col-6 d-flex align-items-center">
+                                <article class="p-5">
+                                    <h3 class="fs-60 fw-600 lh-1">Quality Customer Service</h3>
+                                    <p class="fs-18 pt-3 lh-1_5">
+                                        Our services come at an upfront, affordable price, no surprise or hidden charges involved. We are perfectionists who are proud of our work. We strive to get everything done right the first time, and if someone doesn't agree, we always come back to fix it until everyone is happy, no extra charge. We’re COVID-certified and have been trained in the proper practices. As always, we value safety above all.
+                                    </p>
+                                </article>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+
+        <section class="serviceFaq pt-8">
+            <div class="container">
+                <h3 class="fs-60 fw-700 highlight-secondary text-center">Commercial Plumbing Sydney FAQs</h3>
+                <div class="hot-water-faq-slider pt-6 pt-lg-7 z-1">
+                    <article class="box">
+                        <p class="fs-26 fw-600">What does a hot water heater inspection involve?</p>
+                        <article class="description fs-18 pt-3 lh-1_5">
+                            <p>When we service a hot water system, we inspect and service everything from the ground up. Our comprehensive services include</p>
+                            <ul>
+                                <li>Corrosion inspection</li>
+                                <li>Leak detection</li>
+                                <li>Gas connection inspection</li>
+                                <li>Complete flush</li>
+                            </ul>
+                        </article>
+                    </article>
+                    <article class="box">
+                        <p class="fs-26 fw-600">What are some common problems with hot water systems?</p>
+                        <article class="description fs-18 pt-3 lh-1_5">
+                            <p>Hot water systems are essential to our daily lives, but are also delicate machines. Common ways they may break down include:</p>
+                            <ul>
+                                <li>Failing anode rod no longer dissolving rust in the tank</li>
+                                <li>Bacterial infection causing foul smell in water</li>
+                            </ul>
+                        </article>
+                    </article>
+                    <article class="box">
+                        <p class="fs-26 fw-600">What do I do when my hot water heater stops working?</p>
+                        <article class="description fs-18 pt-3 lh-1_5">
+                            <p>It's time to call in a certified plumber to take a look and make the right repairs. Rapid Plumbing Group's staff are well-trained and highly professional. We work 24/7 and are efficient at any job, so you can be sure that we'll get your hot water systems up and running in no time at all.</p>
+                        </article>
+                    </article>
+                    <article class="box">
+                        <p class="fs-26 fw-600">What should I do first if I have a burst pipe?</p>
+                        <article class="description fs-18 pt-3 lh-1_5">
+                            <p> The damaged section of pipe will have to be replaced. If you are not already well-versed in pipe repair, call in a professional plumber to handle it. In the meantime, turn off the water supply to stop further leakage.</p>
+                        </article>
+                    </article>
+                    <article class="box">
+                        <p class="fs-26 fw-600">Which pipes are most likely to burst?</p>
+                        <article class="description fs-18 pt-3 lh-1_5">
+                            <p>Exposed pipes, old pipes and pipes affected by corrosion are most prone to bursting. This is why it is important to maintain healthy pipes. In cold weather, pipes situated in unheated areas are at more risk.</p>
+                        </article>
+                    </article>
+                    <article class="box">
+                        <p class="fs-26 fw-600">Can I unblock a drain myself?</p>
+                        <article class="description fs-18 pt-3 lh-1_5">
+                            <p>If the drain is not shared and the blockage is minor, it is possible to resolve it with some home remedies.</p>
+                            <ul>
+                                <li>Baking soda and Vinegar</li>
+                                <li>Warm water and Soap</li>
+                            </ul>
+                        </article>
+                    </article>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="services pt-6">
+            <div class="container">
+                <div class="row pt-5">
+                    <div class="col-md-6 col-lg-3">
+                        <article class="box">
+                            <img src="<?= V_CDN_URL . V_THEME_DIR ?>_assets/images/lib/expect-img1.jpg" alt="Rapid Plumbing Van" />
+                            <p class="overlay">Blocked Stormwater Drains</p>
+                        </article>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <article class="box">
+                            <img src="<?= V_CDN_URL . V_THEME_DIR ?>_assets/images/lib/expect-img1.jpg" alt="Rapid Plumbing Van" />
+                            <p class="overlay">Camera Drain Inspections</p>
+                        </article>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <article class="box">
+                            <img src="<?= V_CDN_URL . V_THEME_DIR ?>_assets/images/lib/expect-img1.jpg" alt="Rapid Plumbing Van" />
+                            <p class="overlay">Tree Root Removal</p>
+                        </article>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <article class="box">
+                            <img src="<?= V_CDN_URL . V_THEME_DIR ?>_assets/images/lib/expect-img1.jpg" alt="Rapid Plumbing Van" />
+                            <p class="overlay">Drain Repairs</p>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
+
+    <?php require 'inc/gallery.php'; ?>
 </main>
 
 <?php require 'inc/footer.php'; ?>
