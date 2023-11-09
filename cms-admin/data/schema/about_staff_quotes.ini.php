f0d70c73895a86e974a74c53363cc6e543da33b1
Aayush Rijal <arijal@aiims.com.au>
2023-11-10 10:47:10 +1100

    Service page done

--- cms-admin/data/schema/about_staff_quotes.ini.php
+++ cms-admin/data/schema/about_staff_quotes.ini.php
@@ -21,7 +21,7 @@ return array (
   'listPageFields' => 'dragSortOrder, title',
   'listPageOrder' => 'dragSortOrder DESC',
   'listPageSearchFields' => '_all_',
-  'menuHidden' => '0',
+  'menuHidden' => '1',
   'menuName' => 'About Staff Quotes',
   'menuOrder' => '0000000016',
   'menuPrefixIcon' => '',
