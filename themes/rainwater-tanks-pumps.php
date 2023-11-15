76efa968bbefafc928821cd844c51d6f34439a6e
Aayush Rijal <arijal@aiims.com.au>
2023-11-15 12:09:53 +1100

    rainwater tanks pumps done

--- themes/rainwater-tanks-pumps.php
+++ themes/rainwater-tanks-pumps.php
@@ -41,8 +41,8 @@ require 'inc/serviceBanner.php';
     <section class="help">
         <div class="introduction pb-9">
             <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
+                <div class="row gy-5 ">
+                    <div class="col-lg-6">
                         <article>
                             <h2 class="fw-800 fs-64 lh-1 text-capitalize"><?= _isset($page_content, 'introduction_title') ?></h2>
                             <p class="fw-700 text-primary text-capitalize fs-20 pt-4"><?= _isset($page_content, 'introduction_subtitle') ?></p>
@@ -62,7 +62,7 @@ require 'inc/serviceBanner.php';
                         </article>
                     </div>
 
-                    <div class="col-md-6">
+                    <div class="col-lg-6">
                         <?= _imgSrc($page_content, 'introduction_image', 'h-100 w-100'); ?>
                     </div>
                 </div>
@@ -75,13 +75,13 @@ require 'inc/serviceBanner.php';
                     <div class="col-12">
                         <article class="aboutCard">
                             <div class="row h-100">
-                                <div class="col-6">
+                                <div class="col-lg-6">
                                     <?= _imgSrc($cta, 'image', 'h-100 w-100'); ?>
                                 </div>
-                                <div class="col-6 d-flex align-items-center">
-                                    <article class="p-5">
+                                <div class="col-lg-6 d-flex align-items-center">
+                                <article class="pt-5 pb-3 px-3 p-md-5 px-lg-3 p-xl-5">
                                         <h3 class="fs-60 fw-600 lh-1"><?= _isset($cta, 'title') ?></h3>
-                                        <p class="fs-18 pt-3 lh-1_5">
+                                        <p class="fs-18 pt-3 lh-1_5 description">
                                             <?= _isset($cta, 'content') ?>
                                         </p>
                                     </article>
@@ -96,7 +96,7 @@ require 'inc/serviceBanner.php';
         <section class="serviceFaq pt-8">
             <div class="container">
                 <h3 class="fs-60 fw-700 highlight-secondary text-center"><?= _isset($page_content, 'banner_title') ?> Sydney FAQs</h3>
-                <div class="hot-water-faq-slider pt-6 pt-lg-7 z-1">
+                <div class="innerServiceFaqSlider pt-6 pt-lg-7 z-1">
                     <?php foreach ($faqs as $faq) : ?>
                         <article class="box">
                             <p class="fs-26 fw-600"><?= _isset($faq, 'title') ?></p>
