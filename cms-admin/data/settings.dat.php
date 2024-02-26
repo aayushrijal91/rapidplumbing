7d1536a5479ae70ce1a21b9e8af59de24400578d
Aayush Rijal <arijal@aiims.com.au>
2024-02-26 11:32:49 +1100

    Url structure for main services changed

--- cms-admin/data/settings.dat.php
+++ cms-admin/data/settings.dat.php
@@ -88,7 +88,7 @@ return array (
   'vendorName' => 'interactivetools.com, inc.',
   'vendorUrl' => 'http://www.interactivetools.com/',
   'websiteUrl' => '/rapid',
-  'serverChangeLog_lastCheck' => 1708900720,
+  'serverChangeLog_lastCheck' => 1708906749,
   'serverChangeLog' => '[
     [1693958593,"Server IP","::1"],
     [1693891818,"Server IP","127.0.0.1"],
